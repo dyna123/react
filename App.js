c6951efc5ba7af5147e5d14053c983399eb305be
idham <idhamkholid965@gmail.com>
2019-08-27 14:31:53 +0700

    add search bar

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View, Image } from 'react-native';
+import { Text, StyleSheet, View, Image, TextInput } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <View style={{ backgroundColor: 'blue', flex: 1 }}></View>
+        <View style={{ flex: 1 }}>
+          <View style={{marginHorizontal: 17, flexDirection:'row', paddingTop: 20}}>
+            <View style={{position: 'relative', flex:1}}>
+              <TextInput placeholder="What do you want to eat?" style={{ borderWidth: 1, borderColor: '#e8e8e8', borderRadius: 25, height: 40, fontSize: 13, paddingLeft: 45, paddingRight: 20, backgroundColor: 'white', marginRight: 18 }} />
+              <Image source={require('./assets/search.png')} style={{position: 'absolute', top: 5, left: 12}}/>
+            </View>
+            <View style={{width: 35, alignItems:'center', justifyContent:'center'}}>
+              <Image source={require('./assets/promo.png')}/>
+            </View>
+          </View>
+        </View>
         <View style={{ height: 54, flexDirection: 'row' }}>
           <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: 'white' }}>
             <Image style={{ width: 26, height: 26 }} source={require('./assets/home-active.png')} />
