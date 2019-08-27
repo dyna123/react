a62bf3fd962a120aa2e40fcedd5ea07c19cf2600
idham <idhamkholid965@gmail.com>
2019-08-27 18:46:07 +0700

    add gopay

--- App.js
+++ App.js
@@ -8,13 +8,39 @@ export default class App extends Component {
 
         {/* Search bar */}
         <View style={{ flex: 1 }}>
-          <View style={{marginHorizontal: 17, flexDirection:'row', paddingTop: 20}}>
-            <View style={{position: 'relative', flex:1}}>
+          <View style={{ marginHorizontal: 17, flexDirection: 'row', paddingTop: 20 }}>
+            <View style={{ position: 'relative', flex: 1 }}>
               <TextInput placeholder="What do you want to eat?" style={{ borderWidth: 1, borderColor: '#e8e8e8', borderRadius: 25, height: 40, fontSize: 13, paddingLeft: 45, paddingRight: 20, backgroundColor: 'white', marginRight: 18 }} />
-              <Image source={require('./assets/search.png')} style={{position: 'absolute', top: 5, left: 12}}/>
+              <Image source={require('./assets/search.png')} style={{ position: 'absolute', top: 5, left: 12 }} />
             </View>
-            <View style={{width: 35, alignItems:'center', justifyContent:'center'}}>
-              <Image source={require('./assets/promo.png')}/>
+            <View style={{ width: 35, alignItems: 'center', justifyContent: 'center' }}>
+              <Image source={require('./assets/promo.png')} />
+            </View>
+          </View>
+        </View>
+
+        {/* gopay */}
+        <View style={{ marginHorizontal: 17, marginTop: 8 }}>
+          <View style={{ flexDirection: "row", justifyContent: 'space-between', backgroundColor: '#2c5fb8', borderTopLeftRadius: 4, borderTopRightRadius: 4, padding: 14 }}>
+            <Image source={require('./assets/gopay.png')} />
+            <Text style={{fontSize: 17, fontWeight: 'bold', color:'white'}}>Rp.50375</Text>
+          </View>
+          <View style={{flexDirection: 'row', paddingTop: 20, paddingBottom: 14, backgroundColor: '#2f65bd', borderBottomLeftRadius: 4, borderBottomRightRadius: 4}}>
+             <View style={{flex: 1, alignItems: 'center',}}>
+              <Image source={require('./assets/pay.png')}/>
+              <Text style={{fontWeight: 'bold', fontSize: 13, color:'white', marginTop:15}}>Pay</Text>
+            </View>
+             <View style={{flex: 1, alignItems: 'center',}}>
+              <Image source={require('./assets/nearby.png')}/>
+              <Text style={{fontWeight: 'bold', fontSize: 13, color:'white', marginTop:15}}>Nearby</Text>
+            </View>
+             <View style={{flex: 1, alignItems: 'center',}}>
+              <Image source={require('./assets/topup.png')}/>
+              <Text style={{fontWeight: 'bold', fontSize: 13, color:'white', marginTop:15}}>Top Up</Text>
+            </View>
+             <View style={{flex: 1, alignItems: 'center',}}>
+              <Image source={require('./assets/more.png')}/>
+              <Text style={{fontWeight: 'bold', fontSize: 13, color:'white', marginTop:15}}>More</Text>
             </View>
           </View>
         </View>
